24b2cb71f8b18ad0d21d6ca28cb1b07adb5c39f5
usr <rlarlgus0206@naver.com>
2024-06-15 15:25:59 +0900

    init Commit

---
+++ src/main/java/com/example/digitalsample/DigitalSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.digitalsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/digitalsample/DigitalSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitalsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
