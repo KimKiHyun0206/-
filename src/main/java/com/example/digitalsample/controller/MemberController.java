4afb111487369637ca4d383abce70bbd0d04bd61
Duskafka <rlarlgus0206@naver.com>
2024-08-05 19:38:52 +0900

    update

---
+++ src/main/java/com/example/digitalsample/controller/MemberController.java
@@ -0,0 +1,29 @@
+package com.example.digitalsample.controller;
+
+import com.example.digitalsample.dto.MemberDto;
+import com.example.digitalsample.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RestController
+public class MemberController {
+
+    private final MemberService memberService;
+
+    // 회원가입 API
+    @PostMapping("/join")
+    public Long join(@Valid @RequestBody MemberDto memberDto) {
+        return memberService.join(memberDto);
+    }
+
+    // 로그인 API
+    @PostMapping("/login")
+    public String login(@RequestBody MemberDto memberDto) {
+       return memberService.login(memberDto);
+    }
+}
